767a19ccdc16534dc5fc391c33f8f8a5f00c5b8e
Yuke Zhu <zyk0902@gmail.com>
2017-11-28 17:08:46 -0800

    debugging DDPG agent on halfcheetah

--- surreal/main/ddpg/run_agent.py
+++ surreal/main/ddpg/run_agent.py
@@ -1,13 +1,17 @@
-import sys
 from surreal.agent.ddpg_agent import DDPGAgent
 from surreal.env import *
-from surreal.main.ddpg.configs import *
 from surreal.main.basic_boilerplate import run_agent_main
-
+from surreal.main.ddpg.configs import *
 
 env = gym.make('HalfCheetah-v1')
 env._max_episode_steps = 100
 env = GymAdapter(env)
+
+env = ConsoleMonitor(
+    env,
+    update_interval=10,
+    average_over=10,
+)
 
 run_agent_main(
     agent_class=DDPGAgent,
