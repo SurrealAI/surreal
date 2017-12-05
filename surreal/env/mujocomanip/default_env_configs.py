526dfdda503d80ece7b36627c0b7f50d52e9d99d
jirenz <zhujiren1155@hotmail.com>
2017-12-04 19:52:15 -0800

    add grasping env

--- surreal/env/mujocomanip/default_env_configs.py
+++ surreal/env/mujocomanip/default_env_configs.py
@@ -14,7 +14,7 @@ DEFAULT_SAWYER_ENV_CONFIG = {
 }
 DEFAULT_SAWYER_ENV_CONFIG = extend_config(DEFAULT_SAWYER_ENV_CONFIG, DEFAULT_BASE_MUJOCO_ENV_CONFIG)
 
-DEFAULT_PUSHER_CONFIG =  {
+DEFAULT_SINGLE_OBJECT_TARGET_CONFIG = {
     'mujoco_object_spec': '_dict_',
     'table_size': (0.8, 0.8, 0.8),
     'min_target_xy_distance': (0.1,0.1),
@@ -23,15 +23,36 @@ DEFAULT_PUSHER_CONFIG =  {
     'reward_win': 1,
     'reward_action_norm_factor': 0,
     'reward_objective_factor': 5,
-    'reward_touch_object_factor':0.001,
-    'reward_align_direction_factor':0.001,
     'win_rel_tolerance': 1e-2,
-    'gripper': 'TwoFingerGripper',
+    'gripper': '_str_',
     'obs_spec': {
         'dim': [37],
     }
 }
-DEFAULT_PUSHER_CONFIG = extend_config(DEFAULT_PUSHER_CONFIG, DEFAULT_SAWYER_ENV_CONFIG)
+
+DEFAULT_SINGLE_OBJECT_TARGET_CONFIG = extend_config(DEFAULT_SINGLE_OBJECT_TARGET_CONFIG, DEFAULT_SAWYER_ENV_CONFIG)
+
+DEFAULT_PUSHER_CONFIG =  {
+    'mujoco_object_spec': '_dict_',
+    'min_target_xy_distance': (0.1,0.1),
+    'reward_touch_object_factor':0.001,
+    'reward_align_direction_factor':0.001,
+    'gripper': 'PushingGripper',
+}
+DEFAULT_PUSHER_CONFIG = extend_config(DEFAULT_PUSHER_CONFIG, DEFAULT_SINGLE_OBJECT_TARGET_CONFIG)
+
+DEFAULT_GRASPER_CONFIG =  {
+    'mujoco_object_spec': {
+        'type': 'box',
+        'size': [0.02, 0.02, 0.02],
+        'rgba': [1, 0, 0, 1],
+    },
+    'min_target_xy_distance': (0.1,0.1),
+    'reward_touch_object_factor':0.001,
+    'reward_align_direction_factor':0.001,
+    'gripper': 'TwoFingerGripper',
+}
+DEFAULT_GRASPER_CONFIG = extend_config(DEFAULT_GRASPER_CONFIG, DEFAULT_SINGLE_OBJECT_TARGET_CONFIG)
 
 DEFAULT_STACKER_CONFIG = {
     'mujoco_objects_spec': '_list_',
