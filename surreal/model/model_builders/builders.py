f31fc620f624c3b1aadcf71f12923a0f9d82ecba
jirenz <zhujiren1155@hotmail.com>
2018-01-12 22:12:59 -0800

    roll back model builder

--- surreal/model/model_builders/builders.py
+++ surreal/model/model_builders/builders.py
@@ -7,9 +7,9 @@ class ActorNetwork(nn.Module):
 
     def __init__(self, D_obs, D_act):
         super(ActorNetwork, self).__init__()
-        self.fc_h1 = nn.Linear(D_obs, 100)
-        self.fc_h2 = nn.Linear(100, 100)
-        self.fc_act = nn.Linear(100, D_act)
+        self.fc_h1 = nn.Linear(D_obs, 64)
+        self.fc_h2 = nn.Linear(64, 64)
+        self.fc_act = nn.Linear(64, D_act)
 
     def forward(self, obs):
         h1 = F.relu(self.fc_h1(obs))
@@ -22,20 +22,18 @@ class CriticNetwork(nn.Module):
 
     def __init__(self, D_obs, D_act):
         super(CriticNetwork, self).__init__()
-        self.fc_obs = nn.Linear(D_obs, 100)
-        self.fc_act = nn.Linear(D_act, 100)
+        self.fc_obs = nn.Linear(D_obs, 32)
+        self.fc_act = nn.Linear(D_act, 32)
         # self.fc_h1 = nn.Linear(D_obs+D_act, 128)
-        self.fc_h2 = nn.Linear(100, 100)
-        self.fc_q = nn.Linear(100, 1)
+        self.fc_h2 = nn.Linear(64, 64)
+        self.fc_q = nn.Linear(64, 1)
 
     def forward(self, obs, act):
         h_obs = F.relu(self.fc_obs(obs))
-        h_act = self.fc_act(act)
-        h2_pre = F.relu(self.fc_h2(h_obs))
-        h2 = F.relu(h2_pre + h_act)
+        h_act = F.relu(self.fc_act(act))
         # concat_input = torch.cat((obs, act), 1)
         # h1 = F.relu(self.fc_h1(concat_input))
-        # h1 = torch.cat((h_obs, h_act), 1)
-        # h2 = F.relu(self.fc_h2(h1))
+        h1 = torch.cat((h_obs, h_act), 1)
+        h2 = F.relu(self.fc_h2(h1))
         value = self.fc_q(h2)
         return value
