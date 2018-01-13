a72240e2ef300cf61b17aca95de0bba29ba68756
jirenz <zhujiren1155@hotmail.com>
2018-01-12 16:38:55 -0800

    try smaller model

--- surreal/model/model_builders/builders.py
+++ surreal/model/model_builders/builders.py
@@ -7,9 +7,9 @@ class ActorNetwork(nn.Module):
 
     def __init__(self, D_obs, D_act):
         super(ActorNetwork, self).__init__()
-        self.fc_h1 = nn.Linear(D_obs, 300)
-        self.fc_h2 = nn.Linear(300, 200)
-        self.fc_act = nn.Linear(200, D_act)
+        self.fc_h1 = nn.Linear(D_obs, 100)
+        self.fc_h2 = nn.Linear(100, 100)
+        self.fc_act = nn.Linear(100, D_act)
 
     def forward(self, obs):
         h1 = F.relu(self.fc_h1(obs))
@@ -22,11 +22,11 @@ class CriticNetwork(nn.Module):
 
     def __init__(self, D_obs, D_act):
         super(CriticNetwork, self).__init__()
-        self.fc_obs = nn.Linear(D_obs, 400)
-        self.fc_act = nn.Linear(D_act, 300)
+        self.fc_obs = nn.Linear(D_obs, 100)
+        self.fc_act = nn.Linear(D_act, 100)
         # self.fc_h1 = nn.Linear(D_obs+D_act, 128)
-        self.fc_h2 = nn.Linear(400, 300)
-        self.fc_q = nn.Linear(300, 1)
+        self.fc_h2 = nn.Linear(100, 100)
+        self.fc_q = nn.Linear(100, 1)
 
     def forward(self, obs, act):
         h_obs = F.relu(self.fc_obs(obs))
