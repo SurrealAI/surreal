a7b496d46b6e968279703b335045ded72b6b9b4b
LinxiFan <jimfanspire@gmail.com>
2018-01-30 14:41:47 -0800

    Add git-snapshot tool. Add Kubectl class

--- surreal/kube/git_snapshot.py
+++ surreal/kube/git_snapshot.py
@@ -2,7 +2,7 @@ from git import Repo
 import time
 
 
-def push_snapshot(temp_branch, repo_path='.'):
+def push_snapshot(snapshot_branch, repo_path='.'):
     """
     Save a snapshot of the current codebase (with uncommitted changes and untracked
     files) to a temporary branch and then push to github.
@@ -19,26 +19,35 @@ def push_snapshot(temp_branch, repo_path='.'):
     is_stash = not 'No local changes' in ret
     # delete the temp branch if already exist
     try:
-        git.branch('-D', temp_branch)
+        git.branch('-D', snapshot_branch)
     except:  # the branch doesn't exist, fine.
         pass
-    git.checkout('-b', temp_branch)
+    git.checkout('-b', snapshot_branch)
     if is_stash:
         git.stash('apply')
     git.add('.')
     try:
-        git.commit('-m', 'temporary save ' + time.strftime('%m/%d/%Y %H:%M:%S'))
+        git.commit('-m', 'snapshot save ' + time.strftime('%m/%d/%Y %H:%M:%S'))
     except:
         print('no temporary changes to push')
     try:
-        git.push('-f', 'origin', temp_branch)
+        git.push('-f', 'origin', snapshot_branch)
     except Exception as e:
         print('push to remote failed', e)
     git.checkout(work_branch)
-    git.cherry_pick('-n', temp_branch)
+    git.cherry_pick('-n', snapshot_branch)
     print(git.reset('HEAD'))
 
 
+def main():
+    import argparse
+    parser = argparse.ArgumentParser('Git snapshot tool')
+    parser.add_argument('snapshot_branch', help='name of the snapshot branch')
+    parser.add_argument('repo_path', help='location of the git repository')
+    args = parser.parse_args()
+    push_snapshot(**vars(args))
+
+
 if __name__ == '__main__':
-    import sys
-    push_snapshot(*sys.argv[1:])
+    main()
+
--- surreal/kube/kubectl.py
+++ surreal/kube/kubectl.py
@@ -1,7 +1,11 @@
 """
 Simple python client. The official client is poorly documented and crashes
 on import. Except for `watch`, other info can simply be parsed from stdout.
+
+~/.surreal.yml
+-
 """
+import sys
 import shlex
 import subprocess as pc
 from surreal.kube.yaml_util import YamlList
@@ -17,8 +21,61 @@ def run_process(cmd):
     return out.decode('utf-8'), err.decode('utf-8'), proc.returncode
 
 
+def print_err(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+
+
+class Kubectl(object):
+    def __init__(self, surreal_yml='~/.surreal.yml'):
+        surreal_yml = path.expanduser(surreal_yml)
+        assert path.exists(surreal_yml)
+        self.config = YamlList.from_file(surreal_yml)[0]
+        self.git_config = {
+            'GIT_USER': self.config.git.user,
+            'GIT_TOKEN': self.config.git.token,
+            'GIT_SNAPSHOT_BRANCH': self.config.git['snapshot-branch']
+        }
+
+    def run(self, cmd):
+        return run_process('kubectl ' + cmd)
+
+    def run_verbose(self, cmd):
+        out, err, retcode = self.run(cmd)
+        out, err = out.strip(), err.strip()
+        if retcode != 0:
+            print_err('Kube command error:', retcode)
+            print_err('*' * 20, 'stderr', '*' * 20)
+            print_err(err)
+            print_err('*' * 20, 'stdout', '*' * 20)
+            print_err(out)
+        elif out:
+            print(out)
+
+    def create(self, yaml_file, context=None, **context_kwargs):
+        """
+        kubectl create -f dummy.yml
+
+        Args:
+            yaml_file:
+            context: see `YamlList`
+            **context_kwargs: see `YamlList`
+        """
+        if context or context_kwargs:
+            yaml_list = YamlList.from_template_file(
+                yaml_file,
+                context=context,
+                **context_kwargs)
+            with yaml_list.temp_file() as temp:
+                self.run_verbose('create -f "{}"'.format(temp))
+        else:
+            self.run_verbose('create -f "{}"'.format(yaml_file))
+
+    def create_with_git(self, yaml_file):
+        self.create(yaml_file, context=self.git_config)
+
+
 if __name__ == '__main__':
-    out, err, code = run_process('kubectl config view')
-    print('OUT', out)
-    print('-'*30)
-    print(err)
+    kube = Kubectl()
+    kube.create('~/Dropbox/Portfolio/Kurreal-demo/kpod_gcloud.yml')
+
+
