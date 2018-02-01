b5dc4c3fd88ace89388ccfa4230feed585eaa09c
LinxiFan <jimfanspire@gmail.com>
2018-02-01 13:14:57 -0800

    Add new entry tests, fix shlex quote command

--- installation/entry.py
+++ installation/entry.py
@@ -8,13 +8,14 @@ ENV variables
 """
 import os
 import sys
+import shlex
 import argparse
 import shutil
 import glob
 import errno
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--cmd', type=str, help='run arbitrary command')
+parser.add_argument('--cmd', type=str, nargs='+', help='run arbitrary command')
 parser.add_argument('--sh', type=str, default='', help='shell script')
 parser.add_argument('--bash', action='store_true', help='start interactive bash')
 parser.add_argument('--py', type=str, default='', help='python script')
@@ -35,13 +36,22 @@ def f_copy(fsrc, fdst):
 
 
 def init():
+    """
+    Two ways to pass the mujoco key into the docker container:
+    1. put the mjkey.txt textual string into environment variable `mujoco_key_text`
+    2. mount the mjkey.txt file in /mujoco/
+        docker -ti -v $HOME/.mujoco:/mujoco <docker-image> <...run commands...>
+    """
     os.system('/usr/bin/Xorg -noreset +extension GLX '
               '+extension RANDR +extension RENDER -logfile /etc/fakeX/10.log '
               '-config /etc/fakeX/xorg.conf :10 > /dev/null 2>&1 &')
     mujoco_key = os.environ.get('mujoco_key_text', '')
+    os.system('mkdir -p /root/.mujoco')
     if mujoco_key:
         with open('/root/.mujoco/mjkey.txt', 'w') as fp:
             fp.write(mujoco_key)
+    elif os.path.exists('/mujoco/mjkey.txt'):
+        f_copy('/mujoco/mjkey.txt', '/root/.mujoco')
     else:
         print('WARNING: missing Mujoco `mjkey.txt`')
 
@@ -65,8 +75,9 @@ init()
 if args.bash:
     os.system('bash')
 elif args.cmd:
-    os.system(args.cmd)
+    os.system(' '.join(map(shlex.quote, args.cmd)))
 elif args.py:
+    assert args.py.endswith('.py')
     os.system('python -u ' + args.py)
 elif args.sh:
     os.system('/bin/bash ' + args.bash)
---
+++ installation/test_entry.py
@@ -0,0 +1,4 @@
+import sys
+
+print(sys.argv)
+assert len(sys.argv) == 4, 'entry point shlex quote error'
