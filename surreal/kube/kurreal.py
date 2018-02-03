9e5f555f7f4bc2f74b3f41d43b3e7524b52bc2db
LinxiFan <jimfanspire@gmail.com>
2018-02-02 19:46:43 -0800

    quick update kurreal

--- surreal/kube/kurreal.py
+++ surreal/kube/kurreal.py
@@ -3,23 +3,23 @@ from surreal.kube.generate_command import *
 from surreal.kube.yaml_util import *
 from surreal.utils.serializer import string_hash
 
-# TODO experiment name needs to be passed into command line
-# TODO temp yaml will be saved to surreal-dir/experiment/temp-aosdfjiosdj.yaml
-
 
 experiment_name = 'myexper'
 host_name = string_hash(experiment_name).lower()  # WARNING: must lower case
 service_url = host_name + '.surreal'  # hostname.subdomain
+print('service_url', service_url)
+dry_run = 0
+snapshot = 0
 
 if 1:
-    kube = Kubectl(dry_run=0)
+    kube = Kubectl(dry_run=dry_run)
     gen = CommandGenerator('/mylibs/surreal/surreal/surreal/main/ddpg_configs.py', config_command="--env 'dm_control:cheetah-run' --savefile /root/", service_url=service_url)
     cmd_dict = gen.launch(3)
-    print(cmd_dict['learner'])
+    print(cmd_dict['tensorplex'])
 
     kube.create_surreal(
         './kurreal.yml',
-        snapshot=0,
+        snapshot=snapshot,
         NONAGENT_HOST_NAME=host_name,
         CMD_DICT=cmd_dict
     )
