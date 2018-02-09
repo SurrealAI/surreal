0c74c77e179e0c956a0a4d15d02ae21b950cc2a4
Yuke Zhu <zyk0902@gmail.com>
2018-02-08 17:40:43 -0800

    fix ddpg issues

--- surreal/agent/ddpg_agent.py
+++ surreal/agent/ddpg_agent.py
@@ -24,10 +24,12 @@ class DDPGAgent(Agent):
             agent_id=agent_id,
             agent_mode=agent_mode,
         )
+
         self.action_dim = self.env_config.action_spec.dim[0]
         self.obs_dim = self.env_config.obs_spec.dim[0]
         self.use_z_filter = self.learner_config.algo.use_z_filter
-        
+        self.noise_type = self.learner_config.algo.exploration.noise_type
+        self.sigma = self.learner_config.algo.exploration.sigma
 
         self.model = DDPGModel(
             obs_dim=self.obs_dim,
@@ -37,7 +39,6 @@ class DDPGAgent(Agent):
         
         self.init_noise()
 
-
     def init_noise(self):
         """
             initializes exploration noise
@@ -45,14 +46,14 @@ class DDPGAgent(Agent):
         """
         if self.agent_mode is AgentMode.eval_deterministic:
             return
-        self.noise_type = self.learner_config.algo.exploration.noise_type
         if self.noise_type == 'normal':
-            self.noise = NormalActionNoise(np.zeros(self.action_dim), np.ones(self.action_dim) * self.learner_config.algo.exploration.sigma)
+            self.noise = NormalActionNoise(np.zeros(self.action_dim),
+                                           np.ones(self.action_dim) * self.sigma)
         elif self.noise_type == 'ou_noise':
-            self.noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(self.action_dim), 
-                                                    sigma=self.learner_config.algo.exploration.sigma,
-                                                    theta=self.learner_config.algo.exploration.theta,
-                                                    dt=self.learner_config.algo.exploration.dt)
+            self.noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(self.action_dim),
+                                                      sigma=self.learner_config.algo.exploration.sigma,
+                                                      theta=self.learner_config.algo.exploration.theta,
+                                                      dt=self.learner_config.algo.exploration.dt)
         else:
             raise ConfigError('Noise type {} undefined.'.format(self.noise_type))
 
@@ -65,11 +66,12 @@ class DDPGAgent(Agent):
         assert torch.is_tensor(obs)
         obs = Variable(obs.unsqueeze(0))
         action = self.model.actor(obs).data.numpy()[0]
+        action = action.clip(-1, 1)
 
         if self.agent_mode is not AgentMode.eval_deterministic:
             action += self.noise()
 
-        np.clip(action, -1, 1, out=action)
+        action = action.clip(-1, 1)
         return action
 
     def module_dict(self):
