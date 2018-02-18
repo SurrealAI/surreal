19cbabd82dae20a340a1aa311b55bf8631375df1
LinxiFan <jimfanspire@gmail.com>
2018-02-17 16:42:35 -0800

    Move tensorboard files to its own subdir

--- surreal/main_scripts/tensorboard_wrapper_main.py
+++ surreal/main_scripts/tensorboard_wrapper_main.py
@@ -1,14 +1,15 @@
-import os.path as path
+import os
 import subprocess
+
 
 def tensorboard_parser_setup(parser):
     pass
 
-def run_tensorboardwrapper_main(args, config):
-    folder = config.session_config.folder
-    tensorplex_config = config.session_config.tensorplex
-    cmd = ['tensorboard', '--logdir', folder, '--port', str(tensorplex_config.tensorboard_port)]
-    subprocess.call(cmd)
-    
 
-    
+def run_tensorboardwrapper_main(args, config):
+    folder = os.path.join(config.session_config.folder, 'tensorboard')
+    tensorplex_config = config.session_config.tensorplex
+    cmd = ['tensorboard',
+           '--logdir', folder,
+           '--port', str(tensorplex_config.tensorboard_port)]
+    subprocess.call(cmd)
