f288201ed8b6e145b4f54d631cd5085ae0700196
Orien Zeng <ozeng@stanford.edu>
2018-04-30 13:53:55 -0700

    Put observation.py in

---
+++ surreal/utils/observation.py
@@ -0,0 +1,46 @@
+import torch
+
+def get_matching_keys_for_modality(obs, modality, input_config):
+    '''
+    Extracts observation keys that match the given modality
+    Args:
+        obs: an observation of type OrderedDict, or
+            an observation spec of type OrderedDict
+        modality: an observation modality, such as 'pixel' or 'low_dim'
+        model_config: the model config.  This function uses learner_config.model.input.
+        For example, if the config is
+            'input': {
+                'pixel':['image'],
+                'low_dim':['joint_pos', 'joint_vel'],
+            }
+        and modality is 'pixel'
+        and the observation has keys ['image', 'joint_vel', 'joint_pos', 'x', 'y'],
+        this function will return ['image']
+    '''
+
+    valid_keys = input_config[modality]
+    # Iterate over keys manually to compute intersection of
+    # obs.keys() and valid_keys in order to preserve order
+    matching_keys = []
+    for key in obs.keys():
+        if key in valid_keys:
+            matching_keys.append(key)
+    return matching_keys
+
+def gather_low_dim_input(obs, input_config):
+    matching_keys = get_matching_keys_for_modality(obs,
+                                                  'low_dim',
+                                                  input_config)
+    list_obs_ld = [obs['low_dim'][key] for key in matching_keys]
+    if len(list_obs_ld) < 1: return None
+    obs_low_dim = torch.cat(list_obs_ld, -1)
+    return obs_low_dim
+
+def get_low_dim_shape(obs_spec, input_config):
+    low_dim = 0
+    for key in get_matching_keys_for_modality(obs_spec,
+                                             'low_dim',
+                                              input_config):
+        low_dim += obs_spec['low_dim'][key][0]
+    return low_dim
+
