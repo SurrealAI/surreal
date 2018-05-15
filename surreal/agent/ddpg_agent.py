22e6948703fc0025ddec3c740625dc5f8c375155
Orien Zeng <ozeng@stanford.edu>
2018-05-15 13:45:47 -0700

    Switch to torchx.new_tensor over torch.tensor()
    - This should work as intended with torchx.device_scope and avoid
    inconsistent conversion to cuda tensor

--- surreal/agent/ddpg_agent.py
+++ surreal/agent/ddpg_agent.py
@@ -2,6 +2,7 @@
 Actor function
 """
 import torch
+import torchx ax tx
 import collections
 from .base import Agent
 from surreal.model.ddpg_net import DDPGModel
@@ -84,7 +85,7 @@ class DDPGAgent(Agent):
         for modality in obs:
             modality_dict = collections.OrderedDict()
             for key in obs[modality]:
-                modality_dict[key] = torch.tensor(obs[modality][key], dtype=torch.float32).unsqueeze(0)
+                modality_dict[key] = tx.new_tensor(obs[modality][key], dtype=torch.float32).unsqueeze(0)
             obs_variable[modality] = modality_dict
         action, _ = self.model.forward(obs_variable, calculate_value=False)
         action = action.data.numpy()[0]
