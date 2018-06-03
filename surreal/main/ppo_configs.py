f65e5b67fe87127db9a76f5113a193a96de5c6f5
anchit <anchit.post@gmail.com>
2018-06-03 14:42:58 -0700

    Config for SawyerLift + demonstrations

--- surreal/main/ppo_configs.py
+++ surreal/main/ppo_configs.py
@@ -32,9 +32,9 @@ def generate(argv):
             'experience': 'ExpSenderWrapperMultiStepMovingWindowWithInfo',
             'use_z_filter': False,
             'use_r_filter': False,
-            'gamma': .995, 
-            'n_step': 15, # 10 for without RNN
-            'stride': 10, # 10 for without RNN
+            'gamma': .99, 
+            'n_step': 30, # 10 for without RNN
+            'stride': 20, # 10 for without RNN
             'network': {
                 'lr_actor': 1e-4,
                 'lr_critic': 1e-4,
@@ -48,14 +48,15 @@ def generate(argv):
                     'lr_scheduler': "LinearWithMinLR",
                     'frames_to_anneal': 5e6,
                     'lr_update_frequency': 100, 
-                    'min_lr': 5e-5,
+                    'min_lr': 0.0001,
                 },
             },
+
             # ppo specific parameters:
             'ppo_mode': 'adapt',
             'advantage':{
                 'norm_adv': True,
-                'lam': 0.97,
+                'lam': 1.0,
                 'reward_scale': 1.0,
             },
             'rnn': {
@@ -65,15 +66,15 @@ def generate(argv):
                 'horizon': 5,
             },
             'consts': {
-                'init_log_sig': -2,
-                'log_sig_range': 0.5,
-                'epoch_policy': 10,
-                'epoch_baseline': 10,
+                'init_log_sig': -1.0,
+                'log_sig_range': 0,
+                'epoch_policy': 5,
+                'epoch_baseline': 5,
                 'adjust_threshold': (0.5, 2.0), # threshold to magnify clip epsilon
                 'kl_target': 0.02, # target KL divergence between before and after
             },
             'adapt_consts': {
-                'kl_cutoff_coeff': 50, # penalty coeff when kl large
+                'kl_cutoff_coeff': 500, # penalty coeff when kl large
                 'beta_init': 1.0, # original beta
                 'beta_range': (1/35.0, 35.0), # range of the adapted penalty factor
                 'scale_constant': 1.5,
@@ -101,8 +102,8 @@ def generate(argv):
 
     env_config = {
         'env_name': args.env,
-        'action_repeat': 1,
-        'pixel_input': False,
+        'action_repeat': 10,
+        'pixel_input': True,
         'use_grayscale': False,
         'frame_stacks': 1,
         'sleep_time': 0,
@@ -114,16 +115,16 @@ def generate(argv):
         },
         'observation': {
             'pixel':['camera0'],
-            'low_dim':['proprio', 'low-level'],
-            # 'low_dim':['position', 'velocity','cube_pos', 'cube_quat', 'gripper_to_cube'],
-        },
+            'low_dim':['proprio'],
+            #'low_dim':['position', 'velocity','cube_pos', 'cube_quat', 'gripper_to_cube'],
+        }, 
         'overrides': {
-            'eval': {'use_demonstration': True}
+            'eval': {'use_demonstration': False}
         },
-        'use_demonstration': False,
-        'limit_episode_length': 500,
-        # 'stochastic_eval': True,
-        'stochastic_eval': False,
+        'use_demonstration': True,
+        'limit_episode_length': 200,
+        'stochastic_eval': True,
+        #'stochastic_eval': False,
     }
 
     session_config = Config({
@@ -141,7 +142,7 @@ def generate(argv):
         },
         'agent' : {
             'fetch_parameter_mode': 'step',
-            'fetch_parameter_interval': 100, # 10 for without RNN
+            'fetch_parameter_interval': 250, # 10 for without RNN
             'num_gpus': args.agent_num_gpus,
         },
         'sender': {
