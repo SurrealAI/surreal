d0c7279a15ae7ac87d2215427a53ed283b4b652a
Orien Zeng <ozeng@stanford.edu>
2018-07-08 15:03:12 -0700

    Cheetah config

--- surreal/main/ddpg_configs.py
+++ surreal/main/ddpg_configs.py
@@ -41,7 +41,7 @@ def generate(argv):
             'experience': 'ExpSenderWrapperSSARNStepBootstrap',
             'use_z_filter': False,
             'gamma': .99,
-            'n_step': 6,
+            'n_step': 3,
             'stride': 1,
             'network': {
                 'lr_actor': 1e-4,
@@ -52,8 +52,8 @@ def generate(argv):
                 'critic_gradient_norm_clip': 5.,
                 'actor_regularization': 0.0,
                 'critic_regularization': 0.0,
-                'use_action_regularization': True,
-                'use_double_critic': True,
+                'use_action_regularization': False,
+                'use_double_critic': False,
                 'target_update': {
                     #'type': 'soft',
                     #'tau': 1e-3,
@@ -66,14 +66,13 @@ def generate(argv):
                 'param_noise_sigma': 0.05,
                 'param_noise_alpha': 1.15,
                 'param_noise_target_stddev': 0.005,
-                #'noise_type': 'normal',
+                'noise_type': 'normal',
                 # Agents will be uniformly distributed sigma values from 0.0 to max_sigma.  For example, with 3 agents
                 # The sigma values will be 0.0, 0.33, 0.66
-                'max_sigma': 2.0,
-                'noise_type': 'ou_noise',
-                'theta': 0.15,
-                # 'sigma': 0.3,
-                'dt': 1e-3,
+                'max_sigma': 1.0,
+                #'noise_type': 'ou_noise',
+                #'theta': 0.15,
+                #'dt': 1e-3,
             },
         },
         'replay': {
@@ -95,15 +94,14 @@ def generate(argv):
         'use_demonstration': False,
         'pixel_input': False,
         'use_grayscale': False,
-        'action_repeat': 10,
+        'action_repeat': 1,
         'frame_stacks': 3,
         'frame_stack_concatenate_on_agent': False,
         # Debug only: agent will sleep for this number of seconds between actions
         'sleep_time': 0.0,
-        'limit_episode_length': 200, # 0 means no limit
-        #'limit_episode_length': 0, # 0 means no limit
+        'limit_episode_length': 0, # 0 means no limit
         'video': {
-            'record_video': True,
+            'record_video': False,
             'save_folder': None,
             'max_videos': 500,
             'record_every': 20,
@@ -133,7 +131,7 @@ def generate(argv):
             # fetch_parameter_mode: 'episode', 'episode:<n>', 'step', 'step:<n>'
             # every episode, every n episodes, every step, every n steps
             'fetch_parameter_mode': 'step',
-            'fetch_parameter_interval': 400,
+            'fetch_parameter_interval': 200,
             'num_gpus': args.agent_num_gpus,
         },
         'sender': {
