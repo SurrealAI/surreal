bf4dd04ec6a999c0d430e155be1ce9fb8a416e69
LinxiFan <jimfan@cs.stanford.edu>
2018-10-26 21:56:17 -0700

    remove old doc

--- docs-old/markdown_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# https://stackoverflow.com/questions/9998337/how-to-print-from-github
-# can also use `pip install grip` or `npm install markdown-pdf`
-
-pandoc -V geometry:margin=1in $1.md -f markdown --smart -s -o $1.pdf
