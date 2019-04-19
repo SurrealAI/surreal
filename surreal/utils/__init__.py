a15fddd7f384ea3afdbd146c3cb4474cbd79ae8c
Albert Tung <atung3@stanford.edu>
2019-04-18 20:51:35 -0700

    fix import ordering in utils

--- surreal/utils/__init__.py
+++ surreal/utils/__init__.py
@@ -1,7 +1,7 @@
+from .checkpoint import Checkpoint, PeriodicCheckpoint
+from .config_yaml import get_config_file
 from .common import *
 from .numpy_util import *
 from .schedule import *
 from .filesys import *
 from .serializer import *
-from .checkpoint import Checkpoint, PeriodicCheckpoint
-from .config_yaml import get_config_file
